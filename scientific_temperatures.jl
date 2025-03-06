38b740037617e09deb6dfb624479b614640f3ddc
Haksell <axel.brisse@gmail.com>
2025-03-06 04:24:54 +0100

    .

--- scientific_temperatures.jl
+++ scientific_temperatures.jl
@@ -1,3 +1,10 @@
-function fahrenheit_to_celsius(F)
-	(F - 32) * 5 / 9
+using Test
+
+fahrenheit_to_celsius(f) = (f - 32) * 5 / 9
+
+@testset "Temperature Conversion Tests" begin
+    @test fahrenheit_to_celsius(-40) ≈ -40
+    @test fahrenheit_to_celsius(32) ≈ 0
+    @test fahrenheit_to_celsius(98.6) ≈ 37
+    @test fahrenheit_to_celsius(212) ≈ 100
 end
